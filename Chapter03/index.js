d1b36a258c671f6f160e74db8c4e39d85ec50214
rhildred <rhildred@gmail.com>
2018-10-15 17:50:39 -0400

    got it at least showing a map.

--- Chapter03/index.js
+++ Chapter03/index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/main';
 
-AppRegistry.registerComponent('carBooking', () => App);
+AppRegistry.registerComponent('main', () => App);
